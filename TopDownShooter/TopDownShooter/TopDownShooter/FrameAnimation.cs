8c2f4c017a25ee3dc3a8353a33e09a66d9f9fd3e
Lukasz Biedrzycki <L.Biedr@gmail.com>
2016-11-29 21:51:43 +0100

    Dodanie klasy FrameAnimation

---
+++ TopDownShooter/TopDownShooter/TopDownShooter/FrameAnimation.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace TopDownShooter
+{
+    class FrameAnimation : SpriteManager
+    {
+        public FrameAnimation(Texture2D Texture, int frames)
+            : base(Texture, frames)
+        {
+        }
+
+        public void SetFrame(int frame)
+        {
+            if (frame < Rectangles.Length)
+                FrameIndex = frame;
+        }
+    }
+}
