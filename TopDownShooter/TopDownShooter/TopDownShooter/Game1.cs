f57bbd0dcb685d7209465c8d889b54057ae03607
Lukasz Biedrzycki <L.Biedr@gmail.com>
2016-12-03 00:41:31 +0100

    Dodanie celownika

--- TopDownShooter/TopDownShooter/TopDownShooter/Game1.cs
+++ TopDownShooter/TopDownShooter/TopDownShooter/Game1.cs
@@ -16,6 +16,8 @@ namespace TopDownShooter
         LevelManager levelManager = new LevelManager();
         KeyboardState kbState;
         Camera camera;
+        Texture2D crosshairTexture;
+        MouseState msState;
 
         Vector2 ScreenSize;
 
@@ -23,6 +25,7 @@ namespace TopDownShooter
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            
         }
 
         protected override void Initialize()
@@ -36,7 +39,7 @@ namespace TopDownShooter
 
         protected override void LoadContent()
         {
-            
+            crosshairTexture = Content.Load<Texture2D>("Crosshair");
             spriteBatch = new SpriteBatch(GraphicsDevice);
             levelManager.LoadContent(Content);
         }
@@ -48,7 +51,7 @@ namespace TopDownShooter
         protected override void Update(GameTime gameTime)
         {
             kbState = Keyboard.GetState();
-
+            msState = Mouse.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || kbState.IsKeyDown(Keys.Escape))
                 this.Exit();
 
@@ -66,7 +69,8 @@ namespace TopDownShooter
                 null,null,null,null,
                 camera.transform);
             levelManager.Draw(spriteBatch);
-           
+            spriteBatch.Draw(crosshairTexture, new Vector2(msState.X - crosshairTexture.Width / 2,
+                msState.Y - crosshairTexture.Height / 2), Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
         }
